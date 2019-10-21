cb5b039b57cb8456f1d6dc35287bf958f2af06b7
João Vitor Borges <joao-salves@hotmail.com>
2019-10-21 15:32:03 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department dp = new Department(1, "Books");
-
+        Seller s = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, dp);
+        System.out.println(s);
         System.out.println(dp);
     }
 }
