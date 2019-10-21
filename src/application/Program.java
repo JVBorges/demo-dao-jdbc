321ac9dfcf75357ffb1864d690a030ae0674916d
João Vitor Borges <joao-salves@hotmail.com>
2019-10-21 15:40:38 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,7 @@ public class Program {
     public static void main(String[] args) {
         Department dp = new Department(1, "Books");
         Seller s = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, dp);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(s);
         System.out.println(dp);
     }
