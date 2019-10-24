cb3f69e0f1e7c759aeee982b2614ee177f57e3d4
João Vitor Borges <joao-salves@hotmail.com>
2019-10-24 16:06:58 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,19 +28,24 @@ public class Program {
         list = sellerDao.findAll();
         list.forEach(System.out::println);
         System.out.println();
-        /*
+
         System.out.println("----Seller insert----");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
         System.out.println();
-        */
+
         System.out.println("----Seller update----");
         seller = sellerDao.findById(10);
         seller.setName("Martha Waine");
+        seller.setEmail("martha@gmail.com");
         sellerDao.update(seller);
         System.out.println("Update completed!");
         System.out.println();
+
+        System.out.println("----Seller delete----");
+        sellerDao.deleteById(7);
+        System.out.println("Seller deleted!");
 
     }
 }
